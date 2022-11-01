78346e9ebee53635d65bd1300d8df11d2be7cb56
Dmitry Suvorov <dmitrysuvorov9@gmail.com>
2022-11-02 00:25:50 +0300

    Исправление редей циклов в замыканиях

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -95,7 +95,9 @@ final class MovieQuizViewController: UIViewController {
             message: result.text,
             preferredStyle: .alert)
         
-        let action = UIAlertAction(title: result.buttonText, style: .default) { _ in
+        let action = UIAlertAction(title: result.buttonText, style: .default) { [weak self] _ in
+            guard let self = self else { return }
+            
             self.showFirstQuestion()
         }
         
@@ -124,7 +126,9 @@ final class MovieQuizViewController: UIViewController {
         previewImageView.layer.borderColor = isCorrect ? UIColor.ypGreen.cgColor : UIColor.ypRed.cgColor
         previewImageView.layer.borderWidth = 8
         
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) { [weak self] in
+            guard let self = self else { return }
+            
             self.showNextQuestionOrResults()
         }
     }
