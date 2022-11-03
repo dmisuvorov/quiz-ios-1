a70b48ba1ac9c36e8015d7af38b679186d0f678a
Dmitry Suvorov <dmitrysuvorov9@gmail.com>
2022-11-04 00:08:25 +0300

    Вынесение базовых сущностей бизнес логики из контроллера в отдельные классы

---
+++ MovieQuiz/Models/QuizResultsViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  QuizResultsViewModel.swift
+//  MovieQuiz
+//
+//  Created by Суворов Дмитрий Владимирович on 02.11.2022.
+//
+
+import Foundation
+
+// для состояния "Результат квиза"
+struct QuizResultsViewModel {
+    let title: String
+    let text: String
+    let buttonText: String
+}
---
+++ MovieQuiz/Models/QuizStepViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  QuizStepViewModel.swift
+//  MovieQuiz
+//
+//  Created by Суворов Дмитрий Владимирович on 02.11.2022.
+//
+
+import Foundation
+import UIKit
+
+// для состояния "Вопрос задан"
+struct QuizStepViewModel {
+    let image: UIImage
+    let question: String
+    let questionNumber: String
+}
