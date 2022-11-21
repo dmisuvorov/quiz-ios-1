6331deae8d8411f8a849f29d8dc3f9d379e29afe
Dmitry Suvorov <dmitrysuvorov9@gmail.com>
2022-11-21 12:21:28 +0300

    Модель данных для фильма + загрузчик фильмов MoviesLoader работающий в связке с NetworkClient

---
+++ MovieQuiz/Services/MoviesLoader.swift
@@ -0,0 +1,26 @@
+//
+//  MoviesLoader.swift
+//  MovieQuiz
+//
+//  Created by Суворов Дмитрий Владимирович on 21.11.2022.
+//
+
+import Foundation
+
+struct MoviesLoader: MoviesLoading {
+    // MARK: - NetworkClient
+    private let networkClient = NetworkClient()
+    
+    // MARK: - URL
+    private var mostPopularMoviesUrl: URL {
+        // Если мы не смогли преобразовать строку в URL, то приложение упадёт с ошибкой
+        guard let url = URL(string: "https://imdb-api.com/en/API/Top250Movies/k_i2r51nvx") else {
+            preconditionFailure("Unable to construct mostPopularMoviesUrl")
+        }
+        return url
+    }
+    
+    func loadMovies(handler: @escaping (Result<MostPopularMovies, Error>) -> Void) {
+        
+    }
+}
